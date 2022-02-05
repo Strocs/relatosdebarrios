f5a72d380079c0ed004becd95f398ae0117f39f9
Strocs <iganmolina@gmail.com>
2022-02-05 15:59:52 -0300

    v050222: Size Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import '@styles/home.css'
 
+window.onscroll = function(){
+    let top = window.scrollY;
+    let header = document.getElementsByClassName('header')[0];
 
+    top > 0 ? header.style.visibility = 'visible' : header.style.visibility = 'hidden'
 
-
-
+};
