8bd528c23e61b1f87b473a126f3675b62f64e3e0
Strocs <iganmolina@gmail.com>
2022-03-03 10:32:15 -0300

    Mobile

--- src/js/navFixed.js
+++ src/js/navFixed.js
@@ -1,17 +1,17 @@
 const navFixed = () => {
+  const bar = document.querySelector('.nav')
+  const container = document.querySelector('.nav__container')    
+  const logo = document.querySelector('.nav__logo-img')
     
     window.onscroll = function(){
         let top = window.scrollY;
-        const bar = document.querySelector('.nav')
-        const container = document.querySelector('.nav__container')    
-        const logo = document.querySelector('.nav__logo-img')
 
-        top > 0 ? 	(bar.classList.add('nav__fixed'), 
+        top > 0 ? 	(bar.classList.add('nav__fixed', 'bgBlue'), 
                     container.classList.add('nav__container-fixed'),
                     logo.classList.add('nav__logo-imgFixed')
                     ) 
-                    : 
-                    (bar.classList.remove('nav__fixed'),
+                : 
+                    (bar.classList.remove('nav__fixed', 'bgBlue'),
                     container.classList.remove('nav__container-fixed'),
                     logo.classList.remove('nav__logo-imgFixed')
                     );
