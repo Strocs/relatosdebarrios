25d4700f201c800999a7c0cd79a62740861b2e05
Strocs <iganmolina@gmail.com>
2022-03-07 16:42:09 -0300

    Responsive

--- src/js/displayNav.js
+++ src/js/displayNav.js
@@ -1,6 +1,8 @@
 const displayNav = () => {
     const btn = document.querySelector('.nav__toggle-btn')
+    const btnDisplay = window.getComputedStyle(btn).display
     const nav = document.querySelector('.nav__menu')
+    const menu = document.querySelectorAll('.nav__menu-section')
 
     btn.addEventListener('click', () => {
 
@@ -10,7 +12,12 @@ const displayNav = () => {
 
     })
 
-
+    btnDisplay === 'none'
+    ?   null   
+    :   menu.forEach(list => list.addEventListener('click', () => {
+                nav.classList.remove('setHeight')
+            })
+        )
 }
 
 export default displayNav
